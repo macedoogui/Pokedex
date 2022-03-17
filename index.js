d2d513d3a61e875b9e8821a1c08a9edb9bd6211e
Gui Macedo <98429120+macedoogui@users.noreply.github.com>
2022-03-17 20:13:47 +0000

    section,cards

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get ('/index' , (req , res) =>{
-    res.render('../views/index');
+app.get ('/' , (req , res) =>{;
+    res.render('index');
 
 });
 

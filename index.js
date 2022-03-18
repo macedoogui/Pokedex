23893a40786592485f2c8bdd7fe7af8e25f2af6d
Guilherme <macedoogui@gmail.com>
2022-03-17 22:53:42 -0300

    quase lá

--- index.js
+++ index.js
@@ -4,11 +4,34 @@ const port = 3000;
 const path = require('path');
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+
+const pokedex = [
+    {
+      id: .1,
+      nome: '',      
+      imagem: '',
+      tipo: '',
+      altura: '',
+      peso:'',
+      categoria:'',
+      habilidade:'',
+      descriçao:''
+    }
+]
 
 app.get ('/' , (req , res) =>{;
-    res.render('index');
-
+    res.render('index' , {pokedex});
 });
+
+app.post ('/add' , (req,res) => {
+    const pokemon = req.body;
+    pokedex.push(pokemon)
+
+    res.redirect('/');
+});
+
+
 
 app.listen(port);
 
